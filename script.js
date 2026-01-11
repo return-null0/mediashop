cc1c035116bd59f1fe40a9ab6baa10f6111578f1
Renaldo Hyacinthe <renaldohyacinthe@nyu.edu>
2026-01-11 12:02:18 -0500

    whisper small

--- script.js
+++ script.js
@@ -235,7 +235,7 @@ if (btnCaptions) {
             
             captionStatus.textContent = "Loading AI Model (Whisper Tiny)...";
             
-            const transcriber = await pipeline('automatic-speech-recognition', 'Xenova/distil-whisper-small.en');
+            const transcriber = await pipeline('automatic-speech-recognition', 'Xenova/whisper-small');
             
             captionStatus.textContent = "Transcribing audio...";
             
